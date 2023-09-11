bea2e27048525b95f80138f2ced49e2796378e32
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-09-11 18:24:12 -0300

    add create_booking router

--- app/bookings/dao.py
+++ app/bookings/dao.py
@@ -9,3 +9,68 @@ from app.database import engine, async_session_maker
 
 class BookingDAO(BaseDAO):
     model = Booking
+
+    @classmethod
+    async def insert(cls, user_id: int, room_id: int, date_from: date, date_to: date):
+        # query available rooms
+        """
+        with booked_rooms as (
+            select * from bookings
+            where room_id = 5 and ('2023-09-27' > bookings.date_from and bookings.date_to > '2023-09-20'
+        )
+        select rooms.quantity - COUNT(booked_rooms.room_id) from rooms
+        left join booked_rooms on booked_rooms.room_id = rooms.id
+        where rooms.id = 5
+        group by rooms.quantity, booked_rooms.room_id
+        """
+        async with async_session_maker() as session:
+            booked_rooms = (
+                select(Booking)
+                .where(
+                    and_(
+                        Booking.room_id == room_id,
+                        date_to > Booking.date_from,
+                        Booking.date_to > date_from,
+                    )
+                )
+                .cte("booked_rooms")
+            )
+            rooms_left = (
+                (
+                    select(
+                        (Room.quantity - func.count(booked_rooms.c.room_id)).label(
+                            "rooms_left"
+                        )
+                    )
+                    .select_from(Room)
+                    .join(booked_rooms, booked_rooms.c.room_id == Room.id, isouter=True)
+                )
+                .where(Room.id == room_id)
+                .group_by(Room.quantity, booked_rooms.c.room_id)
+            )
+
+            # print(rooms_left.compile(engine, compile_kwargs={"literal_binds": True}))
+
+            rooms_left = await session.execute(rooms_left)
+            number_rooms_left: int = rooms_left.scalar()
+
+            if number_rooms_left == 0:
+                return None
+            get_price = select(Room.price).filter_by(id=room_id)
+            price = await session.execute(get_price)
+            price: int = price.scalar()
+
+            insert_booking = (
+                insert(Booking)
+                .values(
+                    room_id=room_id,
+                    user_id=user_id,
+                    date_from=date_from,
+                    date_to=date_to,
+                    price=price,
+                )
+                .returning(Booking)
+            )
+            new_booking = await session.execute(insert_booking)
+            await session.commit()
+            return new_booking.scalar()
--- app/bookings/router.py
+++ app/bookings/router.py
@@ -8,7 +8,8 @@ from app.bookings.dao import BookingDAO
 
 from app.bookings.schemas import SBooking, SNewBooking
 
-# from app.exceptions import RoomCannotBeBookedException
+from app.exceptions import RoomCannotBeBookedException
+
 # from app.tasks.tasks import (
 #     send_booking_confirmation,
 #     send_booking_confirmation_with_background_tasks,
@@ -25,6 +26,43 @@ async def get_bookings() -> list[SBooking]:
     return await BookingDAO.fetch_all()
 
 
-@router.get("/")
+@router.get("")
 async def get_bookings(user: User = Depends(get_current_user)) -> list[SBooking]:
     return await BookingDAO.fetch_all(user_id=user.id)
+
+
+@router.post("")
+async def create_booking(
+    # background_tasks: BackgroundTasks,
+    room_id: int,
+    date_from: date,
+    date_to: date,
+    user: User = Depends(get_current_user),
+):
+    new_booking = await BookingDAO.insert(
+        user_id=user.id, room_id=room_id, date_from=date_from, date_to=date_to
+    )
+    if not new_booking:
+        raise RoomCannotBeBookedException
+
+    # # celery
+    # send_booking_confirmation.delay(
+    #     {"date_from": new_booking.date_from, "date_to": new_booking.date_to}, user.email
+    # )
+    #
+    # # background_tasks
+    # background_tasks.add_task(
+    #     send_booking_confirmation_with_background_tasks,
+    #     {"date_from": new_booking.date_from, "date_to": new_booking.date_to},
+    #     user.email,
+    # )
+
+    return new_booking
+
+
+@router.delete("/{booking_id}")
+async def remove_booking(
+    booking_id: int,
+    current_user: User = Depends(get_current_user),
+):
+    await BookingDAO.delete(id=booking_id, user_id=current_user.id)
