b542389cc525944672993bcffb8feb714ddee104
DmitryDubovikov <dmitry.dubovikov@gmail.com>
2023-09-14 11:24:58 -0300

    add basic frontend

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ alembic revision --autogenerate -m 'initial migration'
 
 alembic upgrade head
 
+http://127.0.0.1:8000/pages/hotels?location=Sey&date_from=2023-09-21&date_to=2023-09-28
+
--- app/main.py
+++ app/main.py
@@ -1,16 +1,47 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.bookings.router import router as router_bookings
 from app.users.router import router as router_users
 from app.hotels.router import router as router_hotels
 from app.hotels.rooms.router import router as router_rooms
+from app.pages.router import router as router_pages
+from app.images.router import router as router_images
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="app/static"), "static")
 
 app.include_router(router_users)
 app.include_router(router_bookings)
 app.include_router(router_hotels)
 app.include_router(router_rooms)
+app.include_router(router_pages)
+app.include_router(router_images)
+
+
+# Подключение CORS, чтобы запросы к API могли приходить из браузера
+origins = [
+    # 3000 - порт, на котором работает фронтенд на React.js
+    "http://localhost:3000",
+    "http://test",
+    "/*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=[
+        "Content-Type",
+        "Set-Cookie",
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Origin",
+        "Authorization",
+    ],
+)
 
 
 @app.get("/hello")
